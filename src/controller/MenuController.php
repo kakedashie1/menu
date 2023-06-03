5f6021929efcbce6891efc165daceef9ee2050cf
kakedashie1 <alexatleast1@outlook.jp>
2023-06-03 21:10:49 +0900

    14

--- src/controller/MenuController.php
+++ src/controller/MenuController.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../core/Controller.php';
 
 class MenuController extends Controller
 {
---
+++ src/core/AutoLoader.php
@@ -0,0 +1,27 @@
+<?php
+
+class AutoLoader
+{
+  private $dirs;
+
+  public function register()
+  {
+    spl_autoload_register([$this,'LoadClass']);
+  }
+
+  public function registerDir($dir)
+  {
+    $this->dirs[] = $dir;
+  }
+
+  private function loadClass($className)
+  {
+    foreach ($this->dirs as $dir) {
+      $file = $dir . '/' . $className . '.php';
+      if (is_readable($file)) {
+        require $file;
+        return ;
+      }
+    }
+  }
+}
